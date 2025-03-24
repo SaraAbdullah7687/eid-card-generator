fd487bc4e278e914c96eac5bc73a1376fa9951a5
Nada Al.Qabbani <nada.k.alqabbani@gmail.com>
2025-03-24 11:53:01 +0300

    Eid 2025
    Ramadan

--- app/src/main/App.js
+++ app/src/main/App.js
@@ -1,6 +1,6 @@
 // App.js
 import './App.css';
-import img from '../images/Ramadan_25.png'
+import img from '../images/eid2025.png'
 import React from 'react';
 import html2canvas from 'html2canvas'
 
